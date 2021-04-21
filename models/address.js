193aa401a6fae14a79c1ba5d17907754e6d108b9
Sami Akram <55475314+SamiAkram2812@users.noreply.github.com>
2021-04-22 01:57:25 +0300

    Create address.js

---
+++ models/address.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, Sequelize) => {
+    const Address = sequelize.define("address", {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        customer_id: {
+            type: Sequelize.INTEGER
+        },
+        country: {
+            type: Sequelize.STRING
+        },
+        city: {
+            type: Sequelize.STRING
+        },
+        zip_code: {
+            type: Sequelize.INTEGER
+        },
+        phone: {
+            type: Sequelize.STRING
+        }
+    }, {
+        tableName: "addresses",
+        timestamps: false
+    }
+    );
+
+    return Address;
+};
