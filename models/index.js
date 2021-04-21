0acd2f3c2efd7d67059adb98e42c9d3ada159d68
FatenJaber2000 <82912733+FatenJaber2000@users.noreply.github.com>
2021-04-22 02:00:33 +0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -20,5 +20,6 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 db.customers = require("./customer.js")(sequelize, Sequelize); //add this line
-
+db.products = require("./product.js")(sequelize, Sequelize);
+db.brands = require("./brand.js")(sequelize, Sequelize);
 module.exports = db;
