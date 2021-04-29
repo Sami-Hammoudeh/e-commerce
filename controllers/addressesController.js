aa09ef8e415c1c28a6c11cace4f54947b0c463c8
Sami Akram <sameakram2@gmail.com>
2021-04-29 06:23:58 +0300

    CRUD Addresses

--- controllers/addressesController.js
+++ controllers/addressesController.js
@@ -1,2 +1,26 @@
 const db = require("../models");
 const Address = db.addresses;
+
+exports.addAddress = function (req, res) {
+
+}
+
+exports.getAddress = function (req, res) {
+
+}
+
+exports.getAllAddresses = function (req, res) {
+
+}
+
+exports.deleteAllAddresses = function (req, res) {
+
+}
+
+exports.deleteAddress = function (req, res) {
+
+}
+
+exports.updateAddress = function (req, res) {
+
+}
--- routes/addresses.js
+++ routes/addresses.js
@@ -2,4 +2,11 @@ var express = require('express');
 var router = express.Router();
 const Controller = require('../controllers/addressesController');
 
+router.get('/', Controller.getAllAddresses);
+router.get('/:id', Controller.getAddress);
+router.post('/', Controller.addAddress);
+router.delete('/', Controller.deleteAllAddresses);
+router.delete('/:id', Controller.deleteAddress);
+router.put('/:id', Controller.updateAddress);
+
 module.exports = router;
