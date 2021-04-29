e18ed2a07aede84cebc321f9335ae69e074895ee
Sami Akram <sameakram2@gmail.com>
2021-04-29 06:19:42 +0300

    CRUD Customers

--- routes/customers.js
+++ routes/customers.js
@@ -2,4 +2,11 @@ var express = require('express');
 var router = express.Router();
 const Controller = require('../controllers/customersController');
 
+router.get('/', Controller.getAllUsers);
+router.get('/:id', Controller.getUser);
+router.post('/', Controller.addUser);
+router.delete('/', Controller.deleteAllUsers);
+router.delete('/:id', Controller.deleteUser);
+router.put('/:id', Controller.updateUser);
+
 module.exports = router;
