1256cdee181fc0694dd379e1d439bd98e3ade8db
Sami Akram <sameakram2@gmail.com>
2021-04-29 06:22:13 +0300

    CRUD Admins

--- controllers/adminsController.js
+++ controllers/adminsController.js
@@ -1,2 +1,26 @@
 const db = require("../models");
 const Admin = db.admins;
+
+exports.addAdmin = function (req, res) {
+
+}
+
+exports.getAdmin = function (req, res) {
+
+}
+
+exports.getAllAdmins = function (req, res) {
+
+}
+
+exports.deleteAllAdmins = function (req, res) {
+
+}
+
+exports.deleteAdmin = function (req, res) {
+
+}
+
+exports.updateAdmin = function (req, res) {
+
+}
--- routes/admins.js
+++ routes/admins.js
@@ -2,4 +2,11 @@ var express = require('express');
 var router = express.Router();
 const Controller = require('../controllers/adminsController');
 
+router.get('/', Controller.getAllAdmins);
+router.get('/:id', Controller.getAdmin);
+router.post('/', Controller.addAdmin);
+router.delete('/', Controller.deleteAllAdmins);
+router.delete('/:id', Controller.deleteAdmin);
+router.put('/:id', Controller.updateAdmin);
+
 module.exports = router;
