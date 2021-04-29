7e7ff7ec4db80f3e2eda4e2b33f3aac38ce26943
Sami Akram <sameakram2@gmail.com>
2021-04-29 06:16:08 +0300

    Update customersController.js

--- controllers/customersController.js
+++ controllers/customersController.js
@@ -1,2 +1,26 @@
 const db = require("../models");
 const Customer = db.customers;
+
+exports.addUser = function (req, res) {
+
+}
+
+exports.getUser = function (req, res) {
+
+}
+
+exports.getAllUsers = function (req, res) {
+
+}
+
+exports.deleteAllUsers = function (req, res) {
+
+}
+
+exports.deleteUser = function (req, res) {
+
+}
+
+exports.updateUser = function (req, res) {
+
+}
