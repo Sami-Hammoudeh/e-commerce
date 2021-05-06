de9b02b63482f6b328924f75b6a1fea6727e2051
Sami Akram <sameakram2@gmail.com>
2021-05-06 23:13:03 +0300

    Update addressesController.js

--- controllers/addressesController.js
+++ controllers/addressesController.js
@@ -10,8 +10,31 @@ exports.getAddress = function (req, res) {
 }
 
 exports.getAllAddresses = function (req, res) {
-
-}
+    Address.findAll({
+        attributes: ['content', ['deletedAt','deleteDate']],
+        order: [['createdAt', 'DESC']],
+        include: [
+          { 
+            model: User, // load all users data
+            attributes: ['username']  
+          },
+        ],
+        //paranoid: false //to retrieve even the deleted records
+     })
+       .then(data => {
+         res.send({
+           'data': data,
+           'message': "Addresses retrieved successfully",
+           'status': 200
+         });
+       })
+       .catch(err => {
+         res.status(500).send({
+           message:
+             err.message || "Some error occurred while retrieving Addresses."
+         });
+       });
+    }
 
 exports.deleteAllAddresses = function (req, res) {
 
