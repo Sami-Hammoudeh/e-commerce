ecc3b830ada5b11b5a3d893e2409a35718160d65
FatenJaber2000 <82912733+FatenJaber2000@users.noreply.github.com>
2021-05-18 02:18:02 +0300

    Update brandsController.js

--- controllers/brandsController.js
+++ controllers/brandsController.js
@@ -1,2 +1,26 @@
 const db = require("../models");
 const Brand = db.brands;
+
+exports.addBrand = function (req, res) {
+
+}
+
+exports.getBrand = function (req, res) {
+
+}
+
+exports.getAllBrands = function (req, res) {
+
+}
+
+exports.deleteAllBrands = function (req, res) {
+
+}
+
+exports.deleteBrand = function (req, res) {
+
+}
+
+exports.updateBrand = function (req, res) {
+
+}
