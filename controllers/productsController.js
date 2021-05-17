aa56e84fcb99749c86b7b1832dac71b5451816f7
FatenJaber2000 <82912733+FatenJaber2000@users.noreply.github.com>
2021-05-18 02:18:20 +0300

    Update productsController.js

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,2 +1,26 @@
 const db = require("../models");
 const Product = db.products;
+
+exports.addProduct = function (req, res) {
+
+}
+
+exports.getProduct = function (req, res) {
+
+}
+
+exports.getAllProducts = function (req, res) {
+
+}
+
+exports.deleteAllProducts = function (req, res) {
+
+}
+
+exports.deleteProduct = function (req, res) {
+
+}
+
+exports.updateProduct = function (req, res) {
+
+}
