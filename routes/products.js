cad7f02976b8fb3273e1e6cae66b3adca4d1d444
FatenJaber2000 <82912733+FatenJaber2000@users.noreply.github.com>
2021-05-18 02:10:25 +0300

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -2,4 +2,11 @@ var express = require('express');
 var router = express.Router();
 const Controller = require('../controllers/productsController');
 
+router.get('/', Controller.getAllProducts);
+router.get('/:id', Controller.getProduct);
+router.post('/', Controller.addProduct);
+router.delete('/', Controller.deleteAllProducts);
+router.delete('/:id', Controller.deleteProduct);
+router.put('/:id', Controller.updateProduct);
+
 module.exports = router;
