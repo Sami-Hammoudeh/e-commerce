446698e98ff55ef595519679c23872c9bf018580
FatenJaber2000 <82912733+FatenJaber2000@users.noreply.github.com>
2021-05-18 02:08:31 +0300

    Update brands.js

--- routes/brands.js
+++ routes/brands.js
@@ -2,4 +2,11 @@ var express = require('express');
 var router = express.Router();
 const Controller = require('../controllers/brandsController');
 
+router.get('/', Controller.getAllBrands);
+router.get('/:id', Controller.getBrand);
+router.post('/', Controller.addBrand);
+router.delete('/', Controller.deleteAllBrands);
+router.delete('/:id', Controller.deleteBrand);
+router.put('/:id', Controller.updateBrand);
+
 module.exports = router;
