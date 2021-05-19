15f2148dcd98d26e4e82bae1047b381e4527f281
Sami Akram <sameakram2@gmail.com>
2021-05-20 02:05:06 +0300

    Add Middleware

---
+++ middleware/isAuth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const userController = require("../controllers/userController");
+const db = require("../models");
+const User = db.users;
+// middleware to validate token
+const verifyToken = async (req, res, next) => {
+    const token = req.headers['authorization'];
+    //const token = req.headers['authorization'];
+    if (!token) return res.status(401).json({ error: 'Access denied' });
+    try {
+        const bearer = token.split(" ");
+        const bearerToken = bearer[1];
+        const verified = jwt.verify(bearerToken, process.env.JWT_SECRET);
+
+        //this part to get authorized user data
+        var userdata = userController.parseJwt(bearerToken);
+        const user = await User.findOne({ where: { id: userdata.id } });
+        req.user = user;
+        if (user.id)
+            next()
+        else
+            res.status(300).json({ error: 'You are not authorized' });
+        //
+    } catch (error) {
+        res.status(400).json({ error: 'token not valid' });
+    }
+}
+module.exports = verifyToken
