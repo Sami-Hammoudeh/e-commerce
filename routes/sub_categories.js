0cac25b8c7817033e6d2594d003819d3d3b3227c
Sami Akram <sameakram2@gmail.com>
2021-05-19 19:17:03 +0300

    Update sub_categories.js

--- routes/sub_categories.js
+++ routes/sub_categories.js
@@ -2,4 +2,8 @@ var express = require('express');
 var router = express.Router();
 const Controller = require('../controllers/categoriesController');
 
+//Add Product
+router.post('/:id', Controller.addProduct);
+//Delete Product
+router.delete('/:id', Controller.deleteProduct);
 module.exports = router;
