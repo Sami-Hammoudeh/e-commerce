779a74d751bbc6d0a16fda337291a4c044b45207
Sami Akram <sameakram2@gmail.com>
2021-05-19 21:37:12 +0300

    Update customers.js

--- routes/customers.js
+++ routes/customers.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 const Controller = require('../controllers/customersController');
 
-router.get('/', Controller.getAllCustomers);
+router.get('/', Controller.getAllCustomers); //Admin Auth
 router.get('/:id', Controller.getCustomer);
-router.post('/', Controller.addCustomer);
-router.delete('/', Controller.deleteAllCustomers);
-router.delete('/:id', Controller.deleteCustomer);
+router.delete('/', Controller.deleteAllCustomers); //Admin Auth
+router.delete('/:id', Controller.deleteCustomer); //Admin and Customer Auth
 router.put('/:id', Controller.updateCustomer);
+router.put('/:id/password', Controller.changePassword);
 
 module.exports = router;
