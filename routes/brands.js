c4bb7af44dba4e60454d2d7ff437773cb228c0ca
Sami Akram <sameakram2@gmail.com>
2021-05-19 21:22:04 +0300

    Update brands.js

--- routes/brands.js
+++ routes/brands.js
@@ -9,7 +9,9 @@ router.delete('/', Controller.deleteAllBrands);
 router.delete('/:id', Controller.deleteBrand);
 router.put('/:id', Controller.updateBrand);
 //Add Product
-router.post('/:id', Controller.addProduct);
+router.post('/:id/products', Controller.addProduct);
 //Delete Product
-router.delete('/:id/:product_id', Controller.deleteProduct);
+router.delete('/:id/products/:product_id', Controller.deleteProduct);
+//Get All Products
+router.get('/:id/products', Controller.deleteProduct);
 module.exports = router;
